7c67e56adaeaf3641614bde7f402e57e0accf8c7
itggot-Hugo-Engström-Svanberg <hugo.engstromsvanberg@itggot.se>
2018-04-18 14:45:06 +0200

    added max of four

---
+++ lib/max_of_four.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/max_of_three'
+def max_of_four(num1, num2, num3, num4)
+    max = max_of_three(num1, num2, num3)
+    if max < num4
+        max = num4
+    end
+    return max
+end
